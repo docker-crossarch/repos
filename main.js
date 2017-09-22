04affe42f4c2d2887a94c7dcfbec19fbc858c37f
Marvin ROGER <nivramdu94@gmail.com>
2017-09-22 16:41:00 +0200

    :art: Make output prettier

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import {getTextFromEnv} from './lib/env'
 import {runCommand} from './lib/shell'
 import {generateTempName, createDirectory, copyFile, readFile, writeFile, deleteDirectory} from './lib/fs'
 import {parseSemver} from './lib/semver'
+import {log, info, warning, error} from './lib/log'
 
 const BUILD_FOR_ARCHS = ['amd64', 'armhf']
 const MULTIARCH_ALPINE_BRANCH = 'edge'
@@ -22,9 +23,9 @@ async function main () {
     password: FIELDBOOK_PASSWORD
   }
 
-  console.log(`Building Crossarch images for ${BUILD_FOR_ARCHS.join(', ')} (on top of Alpine ${MULTIARCH_ALPINE_BRANCH})`)
+  info(`Building Crossarch images for ${BUILD_FOR_ARCHS.join(', ')} (on top of Alpine ${MULTIARCH_ALPINE_BRANCH})`)
 
-  console.log('Registering QEMU...')
+  log('Registering QEMU...')
 
   await runCommand('docker', ['run', '--rm', '--privileged', 'multiarch/qemu-user-static:register', '--reset'])
 
@@ -58,7 +59,7 @@ RUN echo "Building image for \${CROSSARCH_ARCH}"`
     const dockerfileContent = await readFile(dockerfilePath)
     await writeFile(dockerfilePath, `${prepend}\n${dockerfileContent}`)
 
-    console.log(`Building ${arch} image...`)
+    log(`Building ${arch} image...`)
 
     await runCommand('docker', ['build', '--no-cache', '-t', `build:${arch}`, tempDir])
     await deleteDirectory(tempDir)
@@ -74,7 +75,7 @@ RUN echo "Building image for \${CROSSARCH_ARCH}"`
 
   const publishedVersionRecord = await getLatestPublishedVersionRecord(fieldbookCreds, BUILD)
   if (publishedVersionRecord.version === version && BUILD !== 'alpine') {
-    console.log('Software not updated since last push - skipping deployment')
+    warning('Software not updated since last push - skipping deployment')
     return
   }
 
@@ -82,15 +83,15 @@ RUN echo "Building image for \${CROSSARCH_ARCH}"`
   * Deployment
   */
 
-  console.log(`Deploying ${BUILD} (${version})`)
+  info(`Deploying ${BUILD} (${version})`)
 
   let semver
   if (BUILD !== 'alpine') {
     semver = parseSemver(version)
-    console.log(`Version major: ${semver.major}, minor: ${semver.minor}, patch: ${semver.patch}`)
+    log(`Version major: ${semver.major}, minor: ${semver.minor}, patch: ${semver.patch}`)
   }
 
-  console.log('Pushing images to Docker Hub...')
+  log('Pushing images to Docker Hub...')
 
   await runCommand('docker', ['login', '-u', DOCKER_USERNAME, '-p', DOCKER_PASSWORD])
   for (const arch of BUILD_FOR_ARCHS) {
@@ -117,13 +118,13 @@ RUN echo "Building image for \${CROSSARCH_ARCH}"`
     }
   }
 
-  console.log('Updating latest published version...')
+  info('Updating latest published version...')
   await setLatestPublishedVersion(fieldbookCreds, publishedVersionRecord, version)
 }
 
 main().then(() => {
-  console.log('Done.')
+  info('Done.')
 }).catch((err) => {
-  console.error(err)
+  error(err.stack)
   process.exit(1)
 })
